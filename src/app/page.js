26abf2c85a8e326d03c80572a2e6ec6f163e00df
pulkitporwal <pulkitporwal.brn@gmail.com>
2024-03-10 17:29:59 +0530

    Home Body Section has been Completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,26 @@
-import HomeHeader from "@/components/HomeHeader"
+import HomeSearch from "@/components/HomeSearch";
+import HomeHeader from "@/components/HomeHeader";
+import Image from "next/image";
 
 const Home = () => {
-  return (
-    <>
-    <HomeHeader/>
-    </>
-  )
-}
+	return (
+		<>
+			<HomeHeader />
+			<div className="flex flex-col items-center mt-24">
+				<Image
+					src={
+						"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+					}
+					alt="Google Logo"
+					width={300}
+					height={100}
+					priority
+					style={{ width: "auto", height: "auto" }}
+				/>
+				<HomeSearch />
+			</div>
+		</>
+	);
+};
 
-export default Home
+export default Home;
